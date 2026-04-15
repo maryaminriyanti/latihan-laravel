0bc6e7b240e5ffeb0f24f33ef9fc7bbcab9b3f47
Maryam Inriyanti <maryam24@mhs.unitama.ac.id>
2026-04-15 20:15:08 +0800

    Pertemuan 4: CRUD

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
-#[Fillable(['name'])]
+#[Fillable(['name', 'nim'])]
 class Student extends Model
 {
     /** @use HasFactory<\Database\Factories\StudentFactory> */
